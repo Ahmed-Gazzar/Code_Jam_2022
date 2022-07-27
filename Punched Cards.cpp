bb77c8650999d05c10b1d9026005e9ecf074f024
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-07-27 16:18:23 +0200

    Update Punched Cards.cpp

--- Punched Cards.cpp
+++ Punched Cards.cpp
@@ -2,12 +2,15 @@
 #include <vector>
 using namespace std;
 
+// a function to generate +-+-+-+ sequence
 void l1(int c){
     for(int i=0;i<c;i++){
         cout << "+-";
     }
     cout << "+" << endl;
 }
+
+// a function to generate |.|.|.|.|.| sequence
 void l2(int c){
     for(int i=0;i<c;i++){
         cout << "|.";
@@ -18,7 +21,8 @@ void l2(int c){
 int main() {
     int t;
     cin >> t;
-    vector<int> rr,cc;
+    vector<int> rr,cc;   // to store the rows and coloumns of the test set
+    
     for(int i=0;i<t;i++){
         int x;
         cin >> x;
@@ -32,10 +36,15 @@ int main() {
         int c = cc[i];
         cout << "Case #" << i+1 << ":" << endl;
         
+        // generating the first tow lines manually cause it has a special beginning
         cout << ".." ;
         l1(c-1);
         cout << ".." ;
         l2(c-1);
+        
+        // generating the rest of the punched card 
+        // noticing that we start with +-+-+-+ and also end with it
+        // so i printed the first +-+-+-+ line the the rest are in the loop
         l1(c);
         
         for(int x=1 ; x < r ; x++){
