28bf12b43ce5ec00c8672e5ba6737cc523259362
Ahmed-Gazzar <85165808+Ahmed-Gazzar@users.noreply.github.com>
2022-07-29 03:31:07 +0200

    Update d1000000 .cpp

--- d1000000 .cpp
+++ d1000000 .cpp
@@ -11,7 +11,7 @@ int main(){
     cin >> t;
     vector<int> res;
 
-    for (int ii = 1; ii <= t; ii++)
+    for (int ii = 1; ii <= t; ii++)  // looping over each test case
     {
         int n;
         cin >> n;
@@ -20,15 +20,24 @@ int main(){
         {
             int z;
             cin >> z;
-            v.push_back(z);
+            v.push_back(z);         // recording the dices of the current test case
         }
-        sort(v.begin(), v.end());
-        int r = 1;
+        
+        /*
+        -   to form a straight, it is a safe move to take the 1 from the smallest dice 
+            and the 2 from the second smallest dice ... etc.
+        -   if the dice does not contain the desired value of the straight 
+            just skip it and move on to the next bigger dice
+        */
+                
+        sort(v.begin(), v.end());   // sorting the dices by their no. of sides
+        
+        int r = 1;                  // first value in the straight
         for (int i = 1; i < n; i++)
         {
-            if (r < v[i]) { r++; }
+            if (r < v[i]) { r++; }  // if the dice contain it, take it. if not, skip it.
         }
-        res.push_back(r);
+        res.push_back(r);           // r now is the largest value of the straight 
 
     }
     
